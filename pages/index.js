813a8e09c54bbfd74fcdb425093c4ca746917877
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-23 12:54:49 -0400

    Create Footer.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Head from "next/head"
 import Avatar from "../components/Avatar"
+import Footer from "../components/Footer"
+import { ViewGridIcon, SearchIcon } from "@heroicons/react/outline"
+import { MicrophoneIcon } from "@heroicons/react/solid"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col  justify-center h-screen'>
       <Head>
         <title>Google 2.0</title>
         <link rel='icon' href='/google.ico' />
@@ -23,6 +28,7 @@ export default function Home() {
           <p className='link'>Images</p>
 
           {/* Icon */}
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
 
           {/* Avatar */}
           <Avatar url='https://pokecharms.com/data/attachment-files/2015/10/236933_Charmander_Picture.png' />
@@ -30,8 +36,28 @@ export default function Home() {
       </header>
 
       {/* Body */}
+      <form className='flex flex-col items-center mt-44 flex-grow'>
+        <Image
+          src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+          width='300'
+          height='100'
+        />
+
+        <div className='flex w-full mt-5 hover:shadow-lg form:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <input type='text' className='focus:outline-none flex-grow' />
+          <MicrophoneIcon className='h-5' />
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4 '>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
 
       {/* Footer */}
+
+      <Footer />
     </div>
   )
 }
