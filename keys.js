7f362c67eda3ddb6ff5037fd2e4e6a5c3267d5b7
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-23 21:35:06 -0400

    Keys

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Keys https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = `AIzaSyAydUmaHEDUzAkck0oUkZLfwSuJ_HLGnLA`
+
+// Context Key https://cse.google.com/create/new
+export const CONTEXT_KEY = `5e3919a32cb790637`
