ddbccc8291c0e775014c8ab614db5b7cf3cc041b
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-23 21:54:20 -0400

    Fixed issue

--- components/Header.js
+++ components/Header.js
@@ -22,6 +22,7 @@ const Header = () => {
   return (
     <header className='sticky top-0 bg-white'>
       <div className='flex w-full p-6 items-center'>
+        
         <Image
           src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
           width={120}
